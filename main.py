860eda6ccd883caefd7cdbfcc18bd23ffbd4763a
Gurpreet Singh <gs3041@mun.ca>
2017-06-14 14:24:20 -0230

    added calling function for bokeh plots for min-max and gdd

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ Created on Tue Jun 13 11:08:45 2017
 
 """
 import Plot_min_max as PM
+import Plot_min_max_bokeh as PM_B
 import Plot_GDD as PDD
+import Plot_GDD_bokeh as PDD_B 
 import Plot_gdd_Q2_1 as Q2
-
+from bokeh import plotting
 
 
 stationid1 = 50092
@@ -17,17 +19,26 @@ stationid2 = 50089
 stationid3 = 6842
 year = 2015
 
+#--------------------------------
 #calling min_max function
 PM.plot_min_max(stationid1, year)
 PM.plot_min_max(stationid2, year)
 PM.plot_min_max(stationid3, year)
+#bokeh
+PM_B.plot_min_max(stationid1, year)
+PM_B.plot_min_max(stationid2, year)
+PM_B.plot_min_max(stationid3, year)
+#-----------------------------------
 
+#----------------------------------
 #calling gdd plot
 PDD.plot_gdd(stationid1, 2015)
 PDD.plot_gdd(stationid1, 2014)
 PDD.plot_gdd(stationid1, 2013)
 PDD.plot_gdd(stationid1, 2012)
-
+#bokeh
+PDD_B.plot_gdd()
+#----------------------------------
 
 #Section 2, question 1
 #Q2.plot_gdd_Q2_1(stationid1, 2015)
