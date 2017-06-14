4d313bdbdab413aed82128adf16d2ac1b9830bbc
Sivachandran Bharathi <bs6523@mun.ca>
2017-06-14 11:12:34 -0230

    '

--- gdd_Cal.py
+++ gdd_Cal.py
@@ -20,12 +20,34 @@ def gdd_tot(mean):
              temp = temp + (i - 10.0)
      return temp
 
-def gdd_cal(stationid, year):
+def gdd_cal_accum(stationid, year):
    
     Citi_name, df_rename = DW.download(stationid, year)
     
     gdd=[]
     for i in range(1,365,31):
         gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][0:i]))
+    
+    return gdd
+
+def gdd_cal(stationid, year):
+   
+    Citi_name, df_rename = DW.download(stationid, year)
+    
+    gdd=[]
+    
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][0:30]))
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][30:60]))
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][60:90]))
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][90:120]))
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][120:150]))
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][150:180]))
+        
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][180:210]))
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][210:240]))
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][240:270]))
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][270:300]))
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][300:330]))
+    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][330:364]))
     
     return gdd
