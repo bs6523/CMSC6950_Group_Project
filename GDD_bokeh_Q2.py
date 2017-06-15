07d21977e9e559f96b8e6286a354755f231558b8
Gurpreet Singh <gs3041@mun.ca>
2017-06-15 13:30:12 -0230

    Changes in code to save plot

--- GDD_bokeh_Q2.py
+++ GDD_bokeh_Q2.py
@@ -1,20 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author gsahans
+
+"""
+#This code shows the bokeh plot showing GDD over selected canadian cities
+
 import gdd_Cal as GD
 import numpy as np
 from bokeh import plotting
+import os
+import download_data as DW
 
 
+def plot_gdd(stationid):
 
-def plot_gdd():
-
-    stationid1 = 50092
-    stationid2 = 50089
-    stationid3 = 6842
+    Citi_name, temp = DW.download(stationid, 2014)
 
     gdd1 = GD.gdd_cal(stationid1, 2015)
     gdd2 = GD.gdd_cal(stationid2, 2014)
     gdd3 = GD.gdd_cal(stationid3, 2013)
+    gdd4 = GD.gdd_cal(stationid, 2016)
 
-    plot=plotting.figure(title="Plot GDD")
+    figure_name = "Bokeh_GDD_{}.html".format(stationid)
+    bok_title="GDD PLOT {}".format(Citi_name)
+
+    plot=plotting.figure(title=bok_title)
     plot.xaxis.axis_label = 'Year/Time'
     plot.yaxis.axis_label = 'Accumlated GDD'
 
@@ -28,5 +39,7 @@ def plot_gdd():
     plot.line(x_data,gdd3, legend="2015", line_color = "green")
     plot.triangle(x_data,gdd3, legend="2013", line_color = "green")
 
+    plot.line(x_data,gdd4, legend="2016", line_color = "orange")
 
+    plotting.output_file(os.path.dirname(os.path.realpath(__file__)) + "/../plots/"+ figure_name, title=bok_title)
     plotting.show(plot)
