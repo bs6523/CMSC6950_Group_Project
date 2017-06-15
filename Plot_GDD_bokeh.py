aa9665116a4901d4733a14f15bc697b93a6bee2d
Gurpreet Singh <gs3041@mun.ca>
2017-06-15 12:46:53 -0230

    Changes in code to save plot

--- Plot_GDD_bokeh.py
+++ Plot_GDD_bokeh.py
@@ -3,23 +3,29 @@
 """
 @author: gsahans (gs3041)
 """
+# This code calculates accumulated GDD vs time for selected canadian cities
+
+
 import gdd_Cal as GD
 import numpy as np
 from bokeh import plotting
-
+import os
+import download_data as DW
 
 
 def plot_gdd():
 
-    stationid1 = 50092
-    stationid2 = 50089
-    stationid3 = 6842
+    Citi_name, temp = DW.download(stationid, 2014)
 
     gdd1 = GD.gdd_cal_accum(stationid1, 2015)
     gdd2 = GD.gdd_cal_accum(stationid2, 2014)
     gdd3 = GD.gdd_cal_accum(stationid3, 2013)
+    gdd4 = GD.gdd_cal_accum(stationid, 2016)
 
-    plot=plotting.figure(title="Plot GDD")
+    figure_name = "Bokeh_GDD-Accum_{}.html".format(stationid)
+    bok_title="GDD PLOT Accum {}".format(Citi_name)
+
+    plot=plotting.figure(title=bok_title)
     plot.xaxis.axis_label = 'Year/Time'
     plot.yaxis.axis_label = 'Accumlated GDD'
 
@@ -33,5 +39,7 @@ def plot_gdd():
     plot.line(x_data,gdd3, legend="2015", line_color = "green")
     plot.triangle(x_data,gdd3, legend="2013", line_color = "green")
 
+    plot.line(x_data,gdd4, legend="2016", line_color = "orange")
 
+    plotting.output_file(os.path.dirname(os.path.realpath(__file__)) + "/../plots/"+ figure_name, title=bok_title)
     plotting.show(plot)
