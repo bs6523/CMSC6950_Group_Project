ed70ea74b948d7be4d5975d99e0be77ef6469c6c
Augustine Uhunoma Osarogiagbon <auo355@mun.ca>
2017-06-15 15:00:59 -0230

    updated to get input from terminal

--- src/gdd_Cal.py
+++ src/gdd_Cal.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Jun 13 14:35:33 2017
 
-@authors: Boxuan Li, Augustine Osarogiagbon
+@authors: Boxuan Li and team members
         
 """
 import sys
@@ -11,49 +11,26 @@ import numpy as np
 import download_data as DW
 import math
 
-input_file = sys.argv[1]
+File_name = sys.argv[1]
 tbase = sys.argv[2]
 tupper = sys.argv[3]
 
 
-
-def gdd_tot(mean):
+def gdd_tot_SB(mean, tbase):
      temp = 0.0
      for i in mean:
          if i < tbase:
              temp = temp + 0.0
          elif not math.isnan(i) :
-             temp = temp + (i - 10.0)
+             temp = temp + (i - tbase)
      return temp
-
-def gdd_cal_accum(stationid, year):
+     
+def gdd_Q1_3(File_name, tbase, tupper):
    
-    Citi_name, df_rename = DW.download(stationid, year)
+    data = pd.read_csv('Data_csv/'+File_name, skiprows=25, sep=",", encoding="ISO-8859-1")
     
     gdd=[]
     for i in range(1,365,31):
-        gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][0:i]))
+        gdd.append(gdd_tot_SB(data['Mean Temp (Â°C)'][0:i], tbase))
     
-    return gdd
-
-def gdd_cal(stationid, year):
-   
-    Citi_name, df_rename = DW.download(stationid, year)
-    
-    gdd=[]
-    
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][0:30]))
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][30:60]))
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][60:90]))
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][90:120]))
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][120:150]))
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][150:180]))
-        
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][180:210]))
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][210:240]))
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][240:270]))
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][270:300]))
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][300:330]))
-    gdd.append(gdd_tot(df_rename['Mean Temp (Â°C)'][330:364]))
-    
-    return gdd
+return gdd 
