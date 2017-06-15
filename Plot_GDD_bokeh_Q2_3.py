e73a958e2304065449b4aafa624e701ec5456af6
Gurpreet Singh <gs3041@mun.ca>
2017-06-15 13:16:41 -0230

    Changes in code to save plot

--- Plot_GDD_bokeh_Q2_3.py
+++ Plot_GDD_bokeh_Q2_3.py
@@ -3,24 +3,30 @@
 """
 @author: Boxuan Li
 """
+
+# This code implements bokeh graph of GDD over T-base for selected canadian cities
+
 import gdd_Cal as GD
 import gdd_Cal_Q2_3 as GDQ23
 import numpy as np
 from bokeh import plotting
-
+import os
+import download_data as DW
 
 
 def plot_gdd():
 
-    stationid1 = 50092
-    stationid2 = 50089
-    stationid3 = 6842
+    Citi_name, temp = DW.download(stationid, 2014)
 
-    gdd1 = GDQ23.gdd_cal_accum_SB(stationid1, 2015, 15)
-    gdd2 = GDQ23.gdd_cal_accum_SB(stationid2, 2014, 15)
-    gdd3 = GDQ23.gdd_cal_accum_SB(stationid3, 2013, 15)
+    gdd1 = GDQ23.gdd_cal_accum_SB(stationid, 2015, 15)
+    gdd2 = GDQ23.gdd_cal_accum_SB(stationid, 2014, 15)
+    gdd3 = GDQ23.gdd_cal_accum_SB(stationid, 2013, 15)
+    gdd4 = GDQ23.gdd_cal_accum_SB(stationid, 2016, 15)
 
-    plot=plotting.figure(title="Plot GDD for Q2_3")
+    figure_name = "Bokeh_GDD_{}.html".format(stationid)
+    bok_title="GDD PLOT T-Base {}".format(Citi_name)
+
+    plot=plotting.figure(title=bok_title)
     plot.xaxis.axis_label = 'Year/Time'
     plot.yaxis.axis_label = 'Accumlated GDD'
 
@@ -34,5 +40,7 @@ def plot_gdd():
     plot.line(x_data,gdd3, legend="2015", line_color = "green")
     plot.triangle(x_data,gdd3, legend="2013", line_color = "green")
 
+    plot.line(x_data,gdd4, legend="2016", line_color = "orange")
 
+    plotting.output_file(os.path.dirname(os.path.realpath(__file__)) + "/../plots/"+ figure_name, title=bok_title)
     plotting.show(plot)
